19968f6bc1ec91c6e906dd0fdda0ba72b37785df
sainathp <pallasainath@gmail.com>
2016-03-21 15:01:48 -0400

    Adding IMS file

--- Inventory_Management_System.py
+++ Inventory_Management_System.py
@@ -1,1 +1,65 @@
 #### IZI Zoo inventory system####
+##install the following packages in your python system
+import os
+import pandas as pd
+import datetime
+##Take user inputs
+def feed_refill():
+    feed_refill_list=[]	
+    feed_refill_date=input("Enter refilling of feed date(mm-dd-yy) in quotes: ")    
+	feed_refill_time=input("Enter refilling of feed time(hh:mm) in quotes: ")
+	feed_refill_zoo=input("Enter refilling zoo number(1-215) in quotes: ")
+	feed_refill_qty=input("Enter refilling quantity(lbs): ")
+	feed_refill_list=[feed_refill_date,feed_refill_time,feed_refill_zoo,feed_refill_qty,'dumped']
+    return feed_refill_list
+def feed_consumed():
+    feed_consumed_list=[]    
+    feed_consumed_date=input("Enter feed consumed date(mm-dd-yy) in quotes: ")
+    feed_consumed_time=input("Enter feed consumed time(hh:mm) in quotes: ")
+    feed_consumed_zoo=input("Enter consumed zoo number(1-215): ")
+    feed_consumed_qty=input("Enter feed consumed in quantity(lbs): ")
+    feed_consumed_animal=input("Enter feeed consumed by animal_number: ")
+    feed_consumed_species=input("Enter feed consumed species category: ")
+    feed_consumed_list=[feed_consumed_date,feed_consumed_time,feed_consumed_zoo,feed_consumed_qty,feed_consumed_animal,feed_consumed_species]
+    return feed_consumed_list
+##Setting paths
+path=os.getcwd()
+os.chdir(path)
+if os.path.isfile('feed_refill_table.csv'):
+    feed_refill_table=pd.read_csv('feed_refill_table.csv')
+else:
+    feed_refill_table=pd.DataFrame(columns=['feed_refill_date','feed_refill_time','feed_refill_zoo','feed_refill_qty','flag'])
+
+if os.path.isfile('feed_consumption_table.csv'):
+    feed_consumption_table=pd.read_csv('feed_consumption_table.csv')
+else:    
+    feed_consumption_table=pd.DataFrame(columns=['feed_consumed_date','feed_consumed_time','feed_consumed_zoo','feed_consumed_qty','feed_consumed_animal','feed_consumed_species'])
+
+if os.path.isfile('wastage_table.csv'):
+    wastage_table=pd.read_csv('wastage_table.csv')
+else:    
+    wastage_table=pd.DataFrame(columns=['wastage_date','wastage_for_zoo','wastage_qty'])
+
+##Main prog
+print 'Welcome to the IZI zoo'
+
+while(True):
+    print '\n'
+    choice=input('\n'"Select your option-" '\n' 
+                 "  Vendor Refill Inventory:1" '\n' 
+                 "  Feed consumption for species:2" '\n' 
+                 "  Reports:3" '\n' 
+                 "  Exit Menu:4"'\n''\n')
+    
+    if(choice==1):
+        n_entry=feed_refill()
+        o_entry=feed_refill_table[feed_refill_table['feed_refill_zoo']==n_entry[2]]
+        if(len(o_entry['flag']=='consumed')>0 or len(o_entry['flag']=='dumped')>0):
+            w_entry=o_entry[-1:].values.flatten()
+            wastage_table=wastage_table.append({'wastage_date':w_entry[0],'wastage_for_zoo':w_entry[2],'wastage_qty':w_entry[3]},ignore_index=True)
+			print '\n''wastage added'
+        feed_refill_table=feed_refill_table.append({'feed_refill_date':n_entry[0],'feed_refill_time':n_entry[1],'feed_refill_zoo':n_entry[2],'feed_refill_qty':n_entry[3],'flag':n_entry[4]},ignore_index=True)
+        print '\n''refill added'    
+    
+    
+
